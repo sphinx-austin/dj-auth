7315123fdc43009ae999452a3aae4650f30897c7
sphinx-austin <austinjuniour4@gmail.com>
2023-09-03 08:16:57 +0300

    update routes

--- users/views.py
+++ users/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# third party imports
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+
+
 
 # Create your views here.
+
+
+# register
+def registerPage(request):
+    form = UserCreationForm()
+
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
+
+# LOGIN
+def loginPage(request):
+    form = UserCreationForm()
+
+    context = {'form':form}
+    return render(request, 'users/login.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def index(request):
     return render(request, 'users/index.html')
