9aea9108961c5c0d3e853b07bd54cbf0b5f49df8
sphinx-austin <austinjuniour4@gmail.com>
2023-09-03 12:28:30 +0300

    add auth

--- users/views.py
+++ users/views.py
@@ -20,40 +20,46 @@ from .forms import CreateUserForm
 
 # register
 def registerPage(request):
-    form = CreateUserForm ()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm ()
 
-    if request.method == 'POST':
-        form = CreateUserForm (request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            success_message = messages.success(request, 'Account successfully created for '+ user)
-            return redirect('login')
+        if request.method == 'POST':
+            form = CreateUserForm (request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                success_message = messages.success(request, 'Account successfully created for '+ user)
+                return redirect('login')
 
-    context = {'form':form}
-    return render(request, 'users/register.html', context)
+        context = {'form':form}
+        return render(request, 'users/register.html', context)
 
 # LOGIN
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username or Password is incorrect!')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect!')
 
-    return render(request, 'users/login.html')
+        return render(request, 'users/login.html')
 
 def logoutUser(request):
     logout(request)
     return redirect('login')
 
 
-@login_required(login_url='login')
+# @login_required(login_url='login')
 def index(request):
     return render(request, 'users/index.html')
