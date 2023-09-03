0634c3b2e51316fdec6a94aa75bb3a3e9b68bb49
sphinx-austin <austinjuniour4@gmail.com>
2023-09-03 08:16:46 +0300

    update routes

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='home'),
+    path("register/", views.registerPage, name='register'),
+    path("login/", views.loginPage, name='login'),
 ]
